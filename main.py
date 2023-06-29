5466c59b329040c55db65ea8c8c8c0e885bcd324
Buddhika Thanura <buddhikajayasingha@gmail.com>
2023-06-29 10:39:40 +0530

    Adding buttons Adding logo Changing fonts Adding Frames

--- main.py
+++ main.py
@@ -1,34 +1,74 @@
 import tkinter as tk
 import customtkinter
 
+MAIN_FONT = "Ubuntu"
+LEFT_BG_COLOR = "#08303b"
+RIGHT_BG_COLOR = "#042430"
+BTN_FG_COLOR = "#0c526b"
+BTN_TXT_COLOR = "White"
+
+passwords_saved = True
+
 #  Creating main app_window
 app = customtkinter.CTk(fg_color="#042430")
 app.geometry("800x600")
 app.title("Password Manager")
-app.configure(padx=25, pady=25)
 
-# app.grid_rowconfigure(1, weight=1)
-# app.grid_columnconfigure(0, weight=1)
 
-canvas = tk.Canvas(app, width=600, height=400, bg='#042430')
 
-#  right pane
-# side_pane = customtkinter.CTkFrame(app, width=500, height=600, fg_color="#031c26", bg_color="#031c26")
-# side_pane.grid(row=0, column=2,  sticky="s")
+# -------Left Pane--------------------
+# Frame
+left_pane = tk.Frame(app)
+left_pane.place(x=0, y=0, width=300, height=600)
+left_pane.configure(bg=LEFT_BG_COLOR)
 
+canvas = tk.Canvas(left_pane, width=100, height=100,  highlightthickness=0, background=LEFT_BG_COLOR)
+password_logo = tk.PhotoImage(file="icons8-password-100.png")
+canvas.create_image(50, 50, image=password_logo)
+canvas.place(x=75, y=25)
 
 # label
-
-welcome_label = customtkinter.CTkLabel(app, text="Welcome Buddhika", fg_color="transparent", font=("Segoe UI", 22))
-welcome_label.place(x=0, y=0)
+welcome_label = customtkinter.CTkLabel(left_pane, text="Welcome Back!", fg_color="transparent", font=(MAIN_FONT, 22))
+welcome_label.place(x=50, y=130)
+welcome_label2 = customtkinter.CTkLabel(left_pane, text="buddhika", font=(MAIN_FONT, 20))
+welcome_label2.place(x=50, y=165)
 
 # buttons
-view_passwords_button = customtkinter.CTkButton(app, text="View Passwords", fg_color="#0c526b", text_color="white")
-view_passwords_button.place(x=0, y=70)
-add_passwords_button = customtkinter.CTkButton(app, text="Add Password", fg_color="#0c526b", text_color="white")
-add_passwords_button.place(x=0, y=110)
-change_email_button = customtkinter.CTkButton(app, text="Change default email", fg_color="#0c526b", text_color="white")
-change_email_button.place(x=0, y=150)
+view_passwords_button = customtkinter.CTkButton(left_pane, text="View Passwords", fg_color=BTN_FG_COLOR, text_color=BTN_TXT_COLOR)
+view_passwords_button.place(x=50, y=225)
+add_passwords_button = customtkinter.CTkButton(left_pane, text="Add Password", fg_color=BTN_FG_COLOR, text_color=BTN_TXT_COLOR)
+add_passwords_button.place(x=50, y=260)
+change_email_button = customtkinter.CTkButton(left_pane, text="Change Default Email", fg_color=BTN_FG_COLOR, text_color=BTN_TXT_COLOR)
+change_email_button.place(x=50, y=295)
+
+
+# -------Right Pane--------------------
+
+# Frame
+right_pane = tk.Frame(app)
+right_pane.place(x=250, y=0, width=550, height=600)
+right_pane.configure(bg=RIGHT_BG_COLOR)
+
+# Title - changing based on password availability
+if passwords_saved:
+    welcome_label = customtkinter.CTkLabel(right_pane, text="Passwords", fg_color="transparent", font=(MAIN_FONT, 22))
+    welcome_label.place(x=250, y=0)
+else:
+    password_title_label = customtkinter.CTkLabel(right_pane, text="Ops!!! There is no passwords to show. Try adding one",
+                                                  fg_color="transparent", font=(MAIN_FONT, 15), text_color="#C0C0C0")
+    password_title_label.place(x=150, y=0)
+
+#  Password list
+
+website_name_label = customtkinter.CTkLabel(right_pane, text="Website Name", fg_color="transparent", font=(MAIN_FONT, 15))
+website_name_label.place(x=150, y=50)
+website_url_label = customtkinter.CTkLabel(right_pane, text="URL", fg_color="transparent", font=(MAIN_FONT, 15))
+website_url_label.place(x=250, y=50)
+website_email_label = customtkinter.CTkLabel(right_pane, text="Email", fg_color="transparent", font=(MAIN_FONT, 15))
+website_email_label.place(x=350, y=50)
+
+
+
 
 
 
